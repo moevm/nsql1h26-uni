96c4f9b1b516b0b6fdb804313d1c2f8d90625627
Kornienko Anton <antosha_kornienko1511@inbox.ru>
2026-04-03 05:07:17 +0300

    university models

---
+++ backend/app/models/university.py
@@ -0,0 +1,18 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class UniversityCreate(BaseModel):
+    name: str
+    city: str
+    has_dormitory: bool
+    military_dept: bool
+    website: str
+    comment: Optional[str] = None
+
+class UniversityUpdate(BaseModel):
+    name: Optional[str] = None
+    city: Optional[str] = None
+    has_dormitory: Optional[bool] = None
+    military_dept: Optional[bool] = None
+    website: Optional[str] = None
+    comment: Optional[str] = None
