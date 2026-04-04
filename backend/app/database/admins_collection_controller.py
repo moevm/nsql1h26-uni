499a6a12113af04dffe1046c1b363f159c92ca3a
Danila-gi <danilaivanovspb@gmail.com>
2026-04-04 13:17:18 +0300

    обновлены методы admins

--- backend/app/database/admins_collection_controller.py
+++ backend/app/database/admins_collection_controller.py
@@ -1,5 +1,5 @@
+from bson import ObjectId
 from pymongo.collection import Collection
-import hashlib
 import datetime
 
 class AdminsCollectionController:
@@ -22,41 +22,49 @@ class AdminsCollectionController:
             print("Error create index:", e)
             return False
 
-    @staticmethod
-    def __get_hash_by_username_password(username: str, password: str) -> str:
-        combinedStr = f"{username}:{password}"
-        hashPassword = hashlib.sha256(combinedStr.encode('utf-8'))
-        return hashPassword.hexdigest()[:60]
-
     def add_admin(self, username: str, password: str) -> str | None:
-        """Возвращает username если удалось добавить админа, иначе None"""
+        """Возвращает id если удалось добавить админа, иначе None"""
         if not self.__check_collection:
             return None
         try:
-            hashPassword = self.__get_hash_by_username_password(username, password)
+
             admin = {
                 "username": username,
-                "password_hash": hashPassword,
+                "password_hash": password,
                 "createdAt": datetime.datetime.now(datetime.timezone.utc)
             }
 
             resultAdd = self.__collection.insert_one(admin)
             if resultAdd.acknowledged:
-                return username
+                return str(resultAdd.inserted_id)
             return None
         except Exception as e:
             print("Error add admin:", e)
             return None
 
-    def find_admin(self, username: str, password: str) -> dict | None:
+    def find_admin_by_username_password(self, username: str, password: str) -> dict | None:
         """Возвращает все поля найденного админа в виде словаря.
-        Или None, если не удалось найти."""
+        Или None, если не удалось найти.
+        _id возвращается в виде ObjectId, так что необходимо перевести его в str."""
         if not self.__check_collection:
             return None
-        hashPassword = self.__get_hash_by_username_password(username, password)
+
         adminFilter = {
             "username": username,
-            "password_hash": hashPassword,
+            "password_hash": password,
+        }
+        adminInDB = self.__collection.find_one(adminFilter)
+        return adminInDB
+
+    def find_admin_by_id(self, idUser: str) -> dict | None:
+        """Возвращает все поля найденного админа в виде словаря. Передавать id в виде str.
+        Или None, если не удалось найти.
+        _id возвращается в виде ObjectId, так что необходимо перевести его в str."""
+        if not self.__check_collection:
+            return None
+
+        adminFilter = {
+            "_id": ObjectId(idUser)
         }
         adminInDB = self.__collection.find_one(adminFilter)
         return adminInDB
